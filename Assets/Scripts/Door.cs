d9cd2a0a9253495580cf7af6fd243836ce613ad2
SkrivanekPeter <peterskrivanek2004@gmail.com>
2026-08-03 06:16:01 +0100

    Added bounds
    Added bounds to every level where necessary
    Bounds referring to invisible tiles with a collider but no ground layer so the player can't wall jump or get out of the map using other means
    Added text for the seals and the door in the hub

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,10 +6,11 @@ public class Door : MonoBehaviour
     // Variables
     [SerializeField] private Player player;
     [SerializeField] private GameManager gameManager;
-    [SerializeField] private string scene;
     [SerializeField] public bool open = false;
     [SerializeField] private Sprite openSprite;
     [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private GameObject destroyText;
+    [SerializeField] private GameObject enterText;
 
     void Start()
     {
@@ -19,23 +20,29 @@ public class Door : MonoBehaviour
         if (gameManager == null)
             gameManager = FindAnyObjectByType<GameManager>();
 
+        StartCoroutine(Delay());
+
         if (player != null)
         {
             if (player.hasDoubleJump && player.hasSprint && player.hasDash && player.hasWallSlide)
             {
                 open = true;
                 spriteRenderer.sprite = openSprite;
+                enterText.SetActive(true);
+                destroyText.SetActive(false);
             }
             else
             {
                 open = false;
+                enterText.SetActive(false);
+                destroyText.SetActive(true);
             }
         }
     }
 
     public void LoadScene()
     {
-        gameManager.LoadScene(scene, Vector2.zero);
+        gameManager.LoadScene("Tomb1", Vector2.zero);
     }
 
     private IEnumerator Delay()
